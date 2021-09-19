8835915a25489c19de2c2407a3f146753f95e51a
chenzhenfei <793385821@qq.com>
2021-09-19 22:58:11 +0800

    [init]

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,21 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
+    <p>{{ flag }}</p>
     <HelloWorld msg="Welcome to Your Vue.js App" />
   </div>
 </template>
 
-<script>
-// @ is an alias to /src
+<script lang="ts">
 import HelloWorld from "@/components/HelloWorld.vue";
-
-export default {
-  name: "Home",
+import { Component, Vue } from "vue-property-decorator";
+@Component({
+  name: "home",
   components: {
     HelloWorld,
   },
-};
+})
+export default class extends Vue {
+  private flag = "fkag";
+}
 </script>
